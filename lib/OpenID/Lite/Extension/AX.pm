309bbfaf0fce45e8e06e4f9ed34501d964bf5512
HIROSE Masaaki <hirose31@gmail.com>
2011-02-17 20:35:03 +0900

    works fine for attribute exchange extension with Google's OpenID.

--- lib/OpenID/Lite/Extension/AX.pm
+++ lib/OpenID/Lite/Extension/AX.pm
@@ -4,11 +4,84 @@ use strict;
 use warnings;
 use base 'Exporter';
 
-our @EXPORT_OK = qw(AX_NS AX_NS_ALIAS);
+our $VERSION = '0.01';
+our @EXPORT_OK = qw(
+                       AX_NS
+                       AX_NS_ALIAS
+                       AX_TYPE_NS_OF
+                  );
 
-use constant AX_NS => q{http://openid.net/src/ax/1.0};
-use constant AX_NS_ALIAS => q{ax};
+use constant AX_NS           => q{http://openid.net/srv/ax/1.0};
+use constant AX_NS_ALIAS     => q{ax};
+use constant AX_TYPE_NS_OF => {
+    country   => q{http://axschema.org/contact/country/home},
+    email     => q{http://axschema.org/contact/email},
+    firstname => q{http://axschema.org/nameperson/first},
+    language  => q{http://axschema.org/pref/language},
+    lastname  => q{http://axschema.org/namePerson/last},
+};
 
 1;
 
+=head1 NAME
 
+OpenID::Lite::Extension::AX - Attribute exchange extension plugin for OpenID::Lite
+
+=head1 SYNOPSIS
+
+RP side
+
+    sub login {
+        ...
+        my $checkid_req = $rp->begin( $identifier )
+            or $your_app->error( $rp->errstr );
+
+        $ax_req = OpenID::Lite::Extension::AX::Request->new;
+        $ax_req->required('email');
+        $checkid_req->add_extension( $ax_req );
+
+        $your_app->redirect_to( $checkid_req->redirect_url( ... ) );
+    }
+
+OP side (!!NOT TESTED!!)
+
+    my $res = $op->handle_request( $your_app->request );
+
+    if ( $res->is_for_setup ) {
+
+        my %option;
+        my $ax_req = OpenID::Lite::Extension::AX::Request->from_provider_response($res);
+        if ($ax_req) {
+            if ($ax_req->mode eq 'fetch_request') {
+                ...
+            }
+        }
+        $your_app->render( %option );
+    }...
+
+=head1 DESCRIPTION
+
+This module is plugin for OpenID::Lite to acomplish Attribute exchange extension flow on easy way.
+
+http://openid.net/specs/openid-attribute-exchange-1_0.html
+http://code.google.com/intl/ja/apis/accounts/docs/OpenID.html
+
+=head1 SEE ALSO
+
+L<OpenID::Lite::Extension::AX::Request>
+
+L<OpenID::Lite::RelyingParty>
+L<OpenID::Lite::Provider>
+
+=head1 AUTHOR
+
+HIROSE Masaaki, E<lt>hirose31@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright HIROSE Masaaki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
--- lib/OpenID/Lite/Extension/AX/Request.pm
+++ lib/OpenID/Lite/Extension/AX/Request.pm
@@ -3,6 +3,55 @@ package OpenID::Lite::Extension::AX::Request;
 use Any::Moose;
 extends 'OpenID::Lite::Extension::Request';
 
+use OpenID::Lite::Extension::AX qw(
+    AX_NS
+    AX_NS_ALIAS
+    AX_TYPE_NS_OF
+);
+
+has 'mode' => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => q{fetch_request},
+);
+
+has 'required' => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => q{country,email,firstname,language,lastname},
+);
+
+override 'append_to_params' => sub {
+    my ( $self, $params ) = @_;
+    $params->register_extension_namespace( AX_NS_ALIAS, AX_NS );
+    $params->set_extension( AX_NS_ALIAS, 'mode', $self->mode );
+    $params->set_extension( AX_NS_ALIAS, 'required', $self->required );
+
+    for my $type (map { lc } split /\s*,\s*/, $self->required) {
+        next unless exists AX_TYPE_NS_OF->{$type};
+        $params->set_extension( AX_NS_ALIAS, "type.${type}", AX_TYPE_NS_OF->{$type} );
+    }
+};
+
+# for OP side
+sub from_provider_response {
+    my ( $class, $res ) = @_;
+    my $message = $res->req_params->copy();
+    my $ns_url  = AX_NS;
+    my $alias   = $message->get_ns_alias($ns_url);
+    return unless $alias;
+    my $data = $message->get_extension_args($alias) || {};
+    my $obj = $class->new();
+    my $result = $obj->parse_extension_args($data);
+    return $result ? $obj : undef;
+}
+
+sub parse_extension_args {
+    my ( $self, $args ) = @_;
+    $self->mode( $args->{mode} ) if $args->{mode};
+    $self->required( $args->{required} ) if $args->{required};
+    return 1;
+}
 
 no Any::Moose;
 __PACKAGE__->meta->make_immutable;
